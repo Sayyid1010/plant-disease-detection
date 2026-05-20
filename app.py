bed8616b60f97199688c115f74a96fd58b0ca49a
Yusuf Gambo <yusufgambo04@gmail.com>
2026-05-20 03:05:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 from PIL import Image
+import tflite_runtime.interpreter as tflite
 
-model = tf.keras.models.load_model("plant_disease_model.h5")
+# Load model
+interpreter = tflite.Interpreter(model_path="plant_disease_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
 classes = ["Tomato Early Blight", "Tomato Late Blight", "Tomato Healthy"]
 
 st.title("🌿 Plant Disease Detection System")
-st.write("Upload a tomato leaf photo to detect the disease instantly!")
+st.write("Upload a tomato leaf photo to detect disease instantly!")
 
 uploaded_file = st.file_uploader("Choose a leaf image...", type=["jpg","jpeg","png"])
 
@@ -15,9 +21,11 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Leaf", width=300)
     img = image.resize((224, 224))
-    img = np.array(img) / 255.0
+    img = np.array(img, dtype=np.float32) / 255.0
     img = np.expand_dims(img, axis=0)
-    prediction = model.predict(img)
+    interpreter.set_tensor(input_details[0]['index'], img)
+    interpreter.invoke()
+    prediction = interpreter.get_tensor(output_details[0]['index'])
     result = classes[np.argmax(prediction)]
     confidence = np.max(prediction) * 100
     st.success(f"Disease Detected: {result}")
